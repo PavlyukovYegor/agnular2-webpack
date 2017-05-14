791466216175c1cb8d885c10af841874557c7542
Unknown <backspaceegor@gmail.com>
2017-05-14 17:23:17 +0300

    new files

---
+++ src/app/randomcontent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'randomcontent-app',
+  templateUrl: 'randomcontent.component.html',
+  styleUrls: ['./randomcontent.component.css']
+})
+export class RandomContentComponent {
+  // images = ["../assets/Angular2.png", "../assets/github.png", "../assets/webpack.png"];
+}
